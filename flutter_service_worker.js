6079ed79708a13427e28897991a6c22558e1eee4
wizwiz900 <83102812+wizwiz900@users.noreply.github.com>
2022-10-19 17:06:42 -0700

    Name Change + Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "416a4b817db57033645dc46b59c64dcf",
-"/": "416a4b817db57033645dc46b59c64dcf",
-"main.dart.js": "ea7063af9b69eb6e5c3828559d1ee948",
+"index.html": "e405d26f101900081a68a307d09e21a0",
+"/": "e405d26f101900081a68a307d09e21a0",
+"main.dart.js": "652c6659f4f41696419386e95b35c24d",
 "manifest.json": "10aa662bd05f36271c6e28968fc02774",
 "version.json": "fc20d6288c809201c140df4ae572b128"
 };
